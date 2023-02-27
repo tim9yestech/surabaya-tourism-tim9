a667e46bea909da8ac7a06bd72113db963415373
Rengga <radistyano@gmail.com>
2023-02-28 00:30:21 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('beranda');
+});
+
+Route::get('/about',function(){
+    return view('about',[
+        "name" => "Rengga Adistyan Orlando",
+        "email" => "radistyano@gmail.com",
+        "image" => "assets/img/itc-surabaya.jpg"
+    ]);
+});
+
+Route::get('/destinasi', function(){
+    return view('destinasi');
+});
+
+Route::get('/umkm', function(){
+    return view ('umkm');
+});
