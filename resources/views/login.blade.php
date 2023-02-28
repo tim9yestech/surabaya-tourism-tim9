64c7f37a4ca2802038757ed55d634334040b94e9
Nurul Maghfiroh <nurma41299@gmail.com>
2023-02-28 07:04:22 +0700

    update

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,8 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="shortcut icon" href="{!! asset('assets/img/Logo-Surabaya.png') !!}">
     <title>Surabaya Tourism | Dinas Kebudayaan, Kepemudaan, dan Olahraga serta Pariwisata</title>
+    {{-- Style --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="{!! asset('css/style2.css') !!}">
 </head>
-<body>
-    
+<body style="background-image: url(assets/img/jembatan-suramadu.jpg)" id="bg-image">
+    <div style="height: 100%; background-color: rgba(194, 85, 216, 0.39)" class="container-fluid"></div>
+    {{-- Bootstrap --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 </html>
