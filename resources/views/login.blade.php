5fedaffee60bd2c136e6f7a9ce9859c0ecf9fc0e
Nurul Maghfiroh <nurma41299@gmail.com>
2023-03-02 05:36:45 +0700

    tamp login fix

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -18,64 +18,69 @@
 </head>
 <body style="background-image: url(assets/img/jembatan-suramadu.jpg)" id="bg-image">
     <div style="height: 100%; background-color: rgba(194, 85, 216, 0.39)" class="container-fluid">
-        <div class="modal modal-signin position-static d-block py-5" tabindex="-1" role="dialog" id="modalSignin">
+        <div class="modal modal-signin position-static d-block py-5" tabindex="-1" role="dialog" id="modalSignin" style="--bs-modal-width: 900px;">
             <div class="modal-dialog" role="document">
                 <div class="modal-content rounded-4 shadow d-flex flex-row padding-box" style="border: none; background-color: #ffffff54;">
-                    <div class="container">
-                        <h4 class="fw-semibold mb-3 text-center">Login Administrator</h4>
-                        <div class="py-1" style="border-top: 3px solid #6868ac;"></div>
-                        <p class="py-2" style="line-height: 1.5; font-size: 14px;">
-                            Surabaya Tourism<br>Dinas Kebudayaan, Kepemudaan dan Olahraga serta Pariwisata Pemerintah Kota Surabaya
-                        </p>
-                        <form action="#" method="POST">
-                            <div class="my-2" style="border: 2px solid #6868ac; border-radius: 3px;">
-                                <div class="wrap-input">
-                                    <label for="email" class="label-input my-2">Email</label>
-                                    <input type="email" id="email" placeholder="name@example.com" class="input shadow">
-                                    <span class="focus-input"></span>
-                                    <span class="symbol-input">
-                                        <i data-feather="mail"></i>
-                                    </span>
-                                </div>
-                                <div class="wrap-input">
-                                    <label for="password" class="label-input my-2">Password</label>
-                                    <input type="password" id="password" placeholder="********" class="input shadow" style="background-color: rgba(255, 255, 255, 0.651);">
-                                    <span class="focus-input"></span>
-                                    <span class="symbol-input">
-                                        <i data-feather="lock"></i>
-                                    </span>
-                                </div>
-                                <div class="wrap-input">
-                                    <label for="role" class="label-input my-2">Role</label>
-                                    <select id="role" class="input shadow" style="border: none;">
-                                        <option>-- Pilih Role --</option>
-                                        <option value="admin">Admin</option>
-                                        <option value="kontributor">Kontributor</option>
-                                    </select>
-                                    <span class="focus-input"></span>
-                                    <span class="symbol-input">
-                                        <i data-feather="user-check"></i>
-                                    </span>
-                                </div>
-                            </div>
-                            <p style="font-weight: 400; font-size: 75%;">
-                                Lupa akun / minat jadi Kontributor? Hubungi <a class="fw-bold" href="https://wa.wizard.id/caf000" target="_blank">tim Programmer</a>. 
+                    <div class="row">
+                        <div class="container col-7">
+                            <img src="{!! asset('assets/img/img-login.jpg') !!}" alt="Wisata Surabaya" style="max-width: 100%; height: 100%; border-radius: 1rem;">
+                        </div>
+                        <div class="container col-5">
+                            <h4 class="fw-semibold mb-3 text-center">Login Administrator</h4>
+                            <div class="py-1" style="border-top: 3px solid #6868ac;"></div>
+                            <p class="py-2" style="line-height: 1.5; font-size: 14px;">
+                                Surabaya Tourism<br>Dinas Kebudayaan, Kepemudaan dan Olahraga serta Pariwisata Pemerintah Kota Surabaya
                             </p>
-                            <div class="row text-center">
-                                <div class="col-6 my-1">
-                                    <button type="submit" class="btn btn-rounded w-100 text-white" style="background: #6868ac; border: 1px solid #6868ac;">
-                                        <i data-feather="log-in" class="mr-1"></i>
-                                        Login
-                                    </button>
+                            <form action="#" method="POST">
+                                <div class="my-2" style="border: 2px solid #6868ac; border-radius: 3px;">
+                                    <div class="wrap-input">
+                                        <label for="email" class="label-input my-2">Email</label>
+                                        <input type="email" id="email" placeholder="name@example.com" class="input shadow">
+                                        <span class="focus-input"></span>
+                                        <span class="symbol-input">
+                                            <i data-feather="mail"></i>
+                                        </span>
+                                    </div>
+                                    <div class="wrap-input">
+                                        <label for="password" class="label-input my-2">Password</label>
+                                        <input type="password" id="password" placeholder="********" class="input shadow" style="background-color: rgba(255, 255, 255, 0.651);">
+                                        <span class="focus-input"></span>
+                                        <span class="symbol-input">
+                                            <i data-feather="lock"></i>
+                                        </span>
+                                    </div>
+                                    <div class="wrap-input">
+                                        <label for="role" class="label-input my-2">Role</label>
+                                        <select id="role" class="input shadow" style="border: none;">
+                                            <option>-- Pilih Role --</option>
+                                            <option value="admin">Admin</option>
+                                            <option value="kontributor">Kontributor</option>
+                                        </select>
+                                        <span class="focus-input"></span>
+                                        <span class="symbol-input">
+                                            <i data-feather="user-check"></i>
+                                        </span>
+                                    </div>
                                 </div>
-                                <div class="col-6 my-1">
-                                    <a href="/" class="btn btn-rounded w-100 btn-style">
-                                    <i data-feather="chevron-left" class="mr-1"></i>
-                                    Halaman Beranda
-                                    </a>
+                                <p style="font-weight: 400; font-size: 75%;">
+                                    Lupa akun / minat jadi Kontributor? Hubungi <a class="fw-bold" href="https://wa.wizard.id/caf000" target="_blank">tim Programmer</a>. 
+                                </p>
+                                <div class="row text-center">
+                                    <div class="col-6 my-1">
+                                        <button type="submit" class="btn btn-rounded w-100 text-white" style="background: #6868ac; border: 1px solid #6868ac;">
+                                            <i data-feather="log-in" class="mr-1"></i>
+                                            Login
+                                        </button>
+                                    </div>
+                                    <div class="col-6 my-1">
+                                        <a href="/" class="btn btn-rounded w-100 btn-style">
+                                        <i data-feather="chevron-left" class="mr-1"></i>
+                                        Beranda
+                                        </a>
+                                    </div>
                                 </div>
-                            </div>
-                        </form>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
