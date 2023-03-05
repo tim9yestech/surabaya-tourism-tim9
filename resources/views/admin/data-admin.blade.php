7f2fa10bce3db4ad991af628f20c305c7bb419c2
viramegantari <vira.meganta29@gmail.com>
2023-03-05 13:33:03 +0700

    update coba

--- resources/views/admin/data-admin.blade.php
+++ resources/views/admin/data-admin.blade.php
@@ -6,4 +6,5 @@
     <p>Ini adalah halaman <span style="color: var(--purple)">{{ $headtitle }}</span></p>
     <p>Selamat membangun web, Maghfiroh! Semoga tuntas sesuai target dengan hasil terbaik dan memuaskan.</p>
     <h1>Apa ya judulnya?</h1>
+    <h2>i don't know</h2>
 @endsection
