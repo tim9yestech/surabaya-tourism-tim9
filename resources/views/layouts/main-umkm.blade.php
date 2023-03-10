c93904f2a2ecdfd2a99a833b1cb5a5cc79dbc0aa
Rengga <radistyano@gmail.com>
2023-03-10 23:31:43 +0700

    update

--- resources/views/layouts/main-umkm.blade.php
+++ resources/views/layouts/main-umkm.blade.php
@@ -10,46 +10,64 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g==" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" integrity="sha512-sMXtMNL1zRzolHYKEujM2AqCLUR9F2C4/05cdbxjjLSRvMQIciEPCQZo++nk7go3BtSuK9kfa/s+a4f4i5pLkw==" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     
 </head>
 <body>
 
     @include('partials.navbar');
-    <div class="page-title">
+    <section class="page-title">
         @yield('page-title')
-    </div>
-    <div class="highlight-image">
+    </section>
+    <section class="highlight-image">
         @yield('highlight-image')
-    </div>
-
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
-        crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"
-        integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw=="
-        crossorigin="anonymous"></script>
-    <script>
-        $('.owl-carousel').owlCarousel({
-            loop: true,
-            margin: 10,
-            nav: true,
-            responsive: {
-                0: {
-                    items: 1
-                },
-                600: {
-                    items: 2
-                },
-                1000: {
-                    items: 3
+        {{-- highlight image start --}}
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
+            crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"
+            integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw=="
+            crossorigin="anonymous"></script>
+        <script>
+            $('.owl-carousel').owlCarousel({
+                loop: true,
+                margin: 10,
+                nav: true,
+                autoplay:true,
+                autoplayTimeout: 4000,
+                autoplayHoverPause:true,
+                responsive: {
+                    0: {
+                        items: 1
+                    },
+                    600: {
+                        items: 2
+                    },
+                    1000: {
+                        items: 3
+                    }
                 }
-            }
-        })
-    </script>
+            });
+            $('.play').on('click',function(){
+            owl.trigger('play.owl.autoplay',[4000])
+            })
+            $('.stop').on('click',function(){
+                owl.trigger('stop.owl.autoplay')
+            })
+        </script>
+    {{-- higlight image end --}}
+    </section>
+
+    <section class="content">
+        @yield('content')
+    </section>
+    <section class="btn_nextpage">
+        @yield('btn_nextpage')
+    </section>
+    @include('partials.footer')
+
+    
 
 </body>
 </html>
--- resources/views/umkm.blade.php
+++ resources/views/umkm.blade.php
@@ -28,10 +28,10 @@
 
 @section('highlight-image')
     <div class="container-fluid">
-        <div class="row mt-5">
-                <div class="owl-carousel owl-theme mb-5">
+        <div class="row mt-3">
+                <div class="owl-carousel owl-theme mb-5 text-center">
                     <div class="item mb-1">
-                        <div class="card border-0 shadow  ">
+                        <div class="head-card shadow">
                             <a href="#">
                                 <img src="assets/img/1.jpg" alt="Lapis Kukus Pahlawawan" class="card-img-top">
                             </a>
@@ -41,7 +41,7 @@
                         </div>
                     </div>
                     <div class="item mb-4">
-                        <div class="card border-0 shadow ">
+                        <div class="head-card border-0 shadow ">
                             <a href="#">
                                 <img src="assets/img/2.jpg" alt="Lapis Kukus Pahlawawan" class="card-img-top">
                             </a>
@@ -51,7 +51,7 @@
                         </div>
                     </div>
                     <div class="item mb-4">
-                        <div class="card border-0 shadow ">
+                        <div class="head-card border-0 shadow ">
                             <a href="#">
                                 <img src="assets/img/3.jpg" alt="Lapis Kukus Pahlawawan" class="card-img-top">
                             </a>
@@ -61,7 +61,7 @@
                         </div>
                     </div>
                     <div class="item mb-4">
-                        <div class="card border-0 shadow ">
+                        <div class="head-card border-0 shadow ">
                             <a href="#">
                                 <img src="assets/img/4.jpg" alt="Lapis Kukus Pahlawawan" class="card-img-top">
                             </a>
@@ -71,7 +71,7 @@
                         </div>
                     </div>
                     <div class="item mb-4">
-                        <div class="card border-0 shadow ">
+                        <div class="head-card border-0 shadow ">
                             <a href="#">
                                 <img src="assets/img/5.jpg" alt="Lapis Kukus Pahlawawan" class="card-img-top">
                             </a>
@@ -81,7 +81,7 @@
                         </div>
                     </div>
                     <div class="item mb-4">
-                        <div class="card border-0 shadow ">
+                        <div class="head-card border-0 shadow ">
                             <a href="#">
                                 <img src="assets/img/6.jpg" alt="Lapis Kukus Pahlawawan" class="card-img-top">
                             </a>
@@ -91,7 +91,7 @@
                         </div>
                     </div>
                     <div class="item mb-4">
-                        <div class="card border-0 shadow ">
+                        <div class="head-card border-0 shadow ">
                             <a href="#">
                                 <img src="assets/img/7.jpg" alt="Lapis Kukus Pahlawawan" class="card-img-top">
                             </a>
@@ -101,7 +101,7 @@
                         </div>
                     </div>
                     <div class="item mb-4">
-                        <div class="card border-0 shadow ">
+                        <div class="head-card border-0 shadow ">
                             <a href="#">
                                 <img src="assets/img/8.jpg" alt="Lapis Kukus Pahlawawan" class="card-img-top">
                             </a>
@@ -111,7 +111,7 @@
                         </div>
                     </div>
                     <div class="item mb-4">
-                        <div class="card border-0 shadow ">
+                        <div class="head-card border-0 shadow ">
                             <a href="#">
                                 <img src="assets/img/9.jpg" alt="Lapis Kukus Pahlawawan" class="card-img-top">
                             </a>
@@ -123,5 +123,104 @@
                 </div>
             </div>
         </div>
+    </div>
+@endsection
+@section('content')
+    <div class="container">
+        <span class="content-title fw-bold">Makanan</span>
+        <div class="row row-cols-1 row-cols-md-4 g-4 mb-5">
+            <div class="col">
+                <div class="card-content">
+                    <img src="assets/img/1.jpg" class="card-img-top" alt="gambar1">
+                </div>
+            </div>
+            <div class="col">
+                <div class="card-content">
+                    <img src="assets/img/2.jpg" class="card-img-top" alt="gambar2">
+                </div>
+            </div>
+            <div class="col">
+                <div class="card-content">
+                    <img src="assets/img/3.jpg" class="card-img-top" alt="gambar3">
+                </div>
+            </div>
+            <div class="col">
+                <div class="card-content">
+                    <img src="assets/img/4.jpg" class="card-img-top" alt="gambar4">
+                </div>
+            </div>
+        </div>
+        <span class="content-title fw-bold">Kerajinan Tangan</span>
+        <div class="row row-cols-1 row-cols-md-4 g-4 mb-5">
+            <div class="col">
+                <div class="card-content">
+                    <img src="assets/img/1.jpg" class="card-img-top" alt="gambar1">
+                </div>
+            </div>
+            <div class="col">
+                <div class="card-content">
+                    <img src="assets/img/2.jpg" class="card-img-top" alt="gambar2">
+                </div>
+            </div>
+            <div class="col">
+                <div class="card-content">
+                    <img src="assets/img/3.jpg" class="card-img-top" alt="gambar3">
+                </div>
+            </div>
+            <div class="col">
+                <div class="card-content">
+                    <img src="assets/img/4.jpg" class="card-img-top" alt="gambar4">
+                </div>
+            </div>
+        </div>
+        <span class="content-title fw-bold">Pakaian</span>
+        <div class="row row-cols-1 row-cols-md-4 g-4 mb-5">
+            <div class="col">
+                <div class="card-content">
+                    <img src="assets/img/1.jpg" class="card-img-top" alt="gambar1">
+                </div>
+            </div>
+            <div class="col">
+                <div class="card-content">
+                    <img src="assets/img/2.jpg" class="card-img-top" alt="gambar2">
+                </div>
+            </div>
+            <div class="col">
+                <div class="card-content">
+                    <img src="assets/img/3.jpg" class="card-img-top" alt="gambar3">
+                </div>
+            </div>
+            <div class="col">
+                <div class="card-content">
+                    <img src="assets/img/4.jpg" class="card-img-top" alt="gambar4">
+                </div>
+            </div>
+        </div>
+        <span class="content-title fw-bold">Kebutuhan Pokok</span>
+        <div class="row row-cols-1 row-cols-md-4 g-4 mb-5">
+            <div class="col">
+                <div class="card-content">
+                    <img src="assets/img/1.jpg" class="card-img-top" alt="gambar1">
+                </div>
+            </div>
+            <div class="col">
+                <div class="card-content">
+                    <img src="assets/img/2.jpg" class="card-img-top" alt="gambar2">
+                </div>
+            </div>
+            <div class="col">
+                <div class="card-content">
+                    <img src="assets/img/3.jpg" class="card-img-top" alt="gambar3">
+                </div>
+            </div>
+            <div class="col">
+                <div class="card-content">
+                    <img src="assets/img/4.jpg" class="card-img-top" alt="gambar4">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="btn-next">
+        <span class="badge badge-primary">Selengkapnya</span>
     </div>
 @endsection
