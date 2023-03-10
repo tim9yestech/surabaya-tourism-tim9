c0669a1b83b896dd2dc63c33dc7950786873ad8d
Rengga <radistyano@gmail.com>
2023-03-10 15:44:50 +0700

    updated

--- resources/views/umkm.blade.php
+++ resources/views/umkm.blade.php
@@ -1,1 +1,127 @@
 @extends('layouts.main-umkm')
+
+@section('page-title')
+    <div class="pt-7">
+        <div class="container">
+        <nav class="mb-4" style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+            <ol class="breadcrumb mb-0 justify-content-center">
+            <li class="breadcrumb-item">
+                <a style="text-decoration:none" href="/">Beranda</a>
+            </li>
+            <li class="breadcrumb-item active" aria-current="page">
+                UMKM
+            </li>
+            </ol>
+        </nav>
+        <div class="row align-items-center justify-content-center">
+            <div class="col-lg-6">
+            <div class="page-title-content">
+                <div class="">
+                <h2 class="text-uppercase font-weight-bold">Produk UMKM</h2>
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
+@endsection
+
+@section('highlight-image')
+    <div class="container-fluid">
+        <div class="row mt-5">
+                <div class="owl-carousel owl-theme mb-5">
+                    <div class="item mb-1">
+                        <div class="card border-0 shadow  ">
+                            <a href="#">
+                                <img src="assets/img/1.jpg" alt="Lapis Kukus Pahlawawan" class="card-img-top">
+                            </a>
+                            <div class="card-body">
+                                <h4> Lapis Kukus Pahlawan</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item mb-4">
+                        <div class="card border-0 shadow ">
+                            <a href="#">
+                                <img src="assets/img/2.jpg" alt="Lapis Kukus Pahlawawan" class="card-img-top">
+                            </a>
+                            <div class="card-body">
+                                <h4> Lapis Kukus Pahlawan</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item mb-4">
+                        <div class="card border-0 shadow ">
+                            <a href="#">
+                                <img src="assets/img/3.jpg" alt="Lapis Kukus Pahlawawan" class="card-img-top">
+                            </a>
+                            <div class="card-body">
+                                <h4> Lapis Kukus Pahlawan</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item mb-4">
+                        <div class="card border-0 shadow ">
+                            <a href="#">
+                                <img src="assets/img/4.jpg" alt="Lapis Kukus Pahlawawan" class="card-img-top">
+                            </a>
+                            <div class="card-body">
+                                <h4> Lapis Kukus Pahlawan</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item mb-4">
+                        <div class="card border-0 shadow ">
+                            <a href="#">
+                                <img src="assets/img/5.jpg" alt="Lapis Kukus Pahlawawan" class="card-img-top">
+                            </a>
+                            <div class="card-body">
+                                <h4> Lapis Kukus Pahlawan</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item mb-4">
+                        <div class="card border-0 shadow ">
+                            <a href="#">
+                                <img src="assets/img/6.jpg" alt="Lapis Kukus Pahlawawan" class="card-img-top">
+                            </a>
+                            <div class="card-body">
+                                <h4> Lapis Kukus Pahlawan</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item mb-4">
+                        <div class="card border-0 shadow ">
+                            <a href="#">
+                                <img src="assets/img/7.jpg" alt="Lapis Kukus Pahlawawan" class="card-img-top">
+                            </a>
+                            <div class="card-body">
+                                <h4> Lapis Kukus Pahlawan</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item mb-4">
+                        <div class="card border-0 shadow ">
+                            <a href="#">
+                                <img src="assets/img/8.jpg" alt="Lapis Kukus Pahlawawan" class="card-img-top">
+                            </a>
+                            <div class="card-body">
+                                <h4> Lapis Kukus Pahlawan</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item mb-4">
+                        <div class="card border-0 shadow ">
+                            <a href="#">
+                                <img src="assets/img/9.jpg" alt="Lapis Kukus Pahlawawan" class="card-img-top">
+                            </a>
+                            <div class="card-body">
+                                <h4> Lapis Kukus Pahlawan</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
